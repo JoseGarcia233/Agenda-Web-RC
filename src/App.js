e64d65db9a1a687928c01630d532181e6aa9c681
JoseGarcia233 <viguera27@gmail.com>
2022-07-08 05:14:30 -0400

    update 1.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './css/App.css';
+import './css/Stable.css';
 import Mform from './components/Mform';
+import Mtable from './components/Mtable';
 
 function App() {
 
   return (
     <>
     <Mform/>
+    <Mtable/>
     </>
   );
 }
---
+++ src/components/Mtable.js
@@ -0,0 +1,24 @@
+
+const table = () => (
+  
+  <div className="container-table">
+  <table class="table">
+  <thead class="thead-info">
+    <tr>
+      <th scope="col">Name</th>
+      <br/>
+      <th scope="col">Last Name</th>
+      <br/>
+      <th scope="col">Number Phone</th>
+      <br/>
+    </tr>
+  </thead>
+  </table>
+  
+  </div>
+
+
+
+);
+
+export default table;
